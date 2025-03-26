f69b21d2076b6a89454aad811181660f36af6cda
Joadison <Joadison@users.noreply.github.com>
2025-03-26 13:34:39 -0300

    Correct

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Eventos de Adoração e Comunhão - 5 de Abril",
-  description: "Tarde de Avivamento e Culto da Juventude TC - Venha viver uma experiência única!",
-}
+  description:
+    "Tarde de Avivamento e Culto da Juventude TC - Venha viver uma experiência única!",
+};
 
 export default function RootLayout({
   children,
@@ -26,11 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${inter.className}`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
